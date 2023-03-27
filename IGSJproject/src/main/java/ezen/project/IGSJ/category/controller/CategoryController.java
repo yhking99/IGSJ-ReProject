d84944eb4fca843002bbcbac6153378e0165cbb4
Auden Kim <sasameyuki03@naver.com>
2023-03-27 20:58:04 +0900

    2023-03-27 (재국) 관리자 페이지 최종관리자 부분 작업

--- IGSJproject/src/main/java/ezen/project/IGSJ/category/controller/CategoryController.java
+++ IGSJproject/src/main/java/ezen/project/IGSJ/category/controller/CategoryController.java
@@ -29,9 +29,11 @@ public class CategoryController {
 	@GetMapping("/items/{num}")
 	@ResponseBody
 	public List<CategoryDTO> getCategoryInfos(@PathVariable int num) throws Exception {
+		
 		System.out.println(num);
 		List<CategoryDTO> categoryInfos = categoryService.getCategoryInfos(num);
 		return categoryInfos;
+		
 	} // getCategoryInfos
 
 
