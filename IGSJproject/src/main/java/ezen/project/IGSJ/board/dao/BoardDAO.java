847d55010469da4989d61716704e797ea28ebcfe
Auden Kim <sasameyuki03@naver.com>
2023-04-03 16:27:09 +0900

    2023-04-03 (재국) 이제 나도 들어간다 vue.js!!! 공지사항, 문의 작업중

--- IGSJproject/src/main/java/ezen/project/IGSJ/board/dao/BoardDAO.java
+++ IGSJproject/src/main/java/ezen/project/IGSJ/board/dao/BoardDAO.java
@@ -1,5 +1,11 @@
 package ezen.project.IGSJ.board.dao;
 
-public interface BoardDAO {
+import java.util.List;
 
+import ezen.project.IGSJ.board.domain.BoardDTO;
+
+public interface BoardDAO {
+	
+	// 공지사항 리스트 가져오기
+	public List<BoardDTO> getBoardList() throws Exception;
 }
