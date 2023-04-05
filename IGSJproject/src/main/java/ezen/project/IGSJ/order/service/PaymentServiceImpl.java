28ee082a1d730e63510e2e80f05a852eaa3fbbc9
DESKTOP-2T2DJ9V\jny09 <jung9993@naver.com>
2023-04-05 09:33:40 +0900

    2023-04-05(나윤) 결제 구현중

--- IGSJproject/src/main/java/ezen/project/IGSJ/order/service/PaymentServiceImpl.java
+++ IGSJproject/src/main/java/ezen/project/IGSJ/order/service/PaymentServiceImpl.java
@@ -1,5 +1,30 @@
 package ezen.project.IGSJ.order.service;
 
-public class PaymentServiceImpl {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import ezen.project.IGSJ.order.dao.PaymentDAO;
+import ezen.project.IGSJ.order.domain.OrderDTO;
+import ezen.project.IGSJ.order.domain.OrderDetailDTO;
+import ezen.project.IGSJ.order.domain.PaymentDTO;
+
+@Service
+public class PaymentServiceImpl implements PaymentService{
+
+	private static final Logger logger = LoggerFactory.getLogger(PaymentServiceImpl.class);
+	
+	@Autowired
+	private PaymentDAO paymentDAO;
+	
+	//결제
+	@Override
+	public void pay(String userId, OrderDTO orderDTO, OrderDetailDTO orderDetailDTO, PaymentDTO paymentDTO) throws Exception{
+		
+		logger.info("결제 pay - paymentService");
+		
+		paymentDAO.pay(userId, orderDTO, orderDetailDTO, paymentDTO);
+	}
+	
 }
